0b2db9769ad6a0e86cd1cc4bb14bcfa52ab45a97
Joseph Dykstra <josephdykstra@gmail.com>
2014-10-16 17:58:57 -0500

    Reformatted tests

--- test/testFactors.js
+++ test/testFactors.js
@@ -4,21 +4,21 @@ var aMath = require('../index.js')
 test('Check the factors function', function(t) {
 	var testThese = [2, 7, 15, 16, 23, 77, 94, 95, 96, 97, 98, 99, 100, 376]
 	var checkAgainst = [
-			[2],
-			[7],
-			[3, 5],
-			[2, 2, 2, 2],
-			[23],
-			[7, 11],
-			[2, 47],
-			[5, 19],
-			[2, 2, 2, 2, 2, 3],
-			[97],
-			[2, 7, 7],
-			[3, 3, 11],
-			[2, 2, 5, 5],
-			[2, 2, 2, 47]
-		]
+		[2],
+		[7],
+		[3, 5],
+		[2, 2, 2, 2],
+		[23],
+		[7, 11],
+		[2, 47],
+		[5, 19],
+		[2, 2, 2, 2, 2, 3],
+		[97],
+		[2, 7, 7],
+		[3, 3, 11],
+		[2, 2, 5, 5],
+		[2, 2, 2, 47]
+	]
 	testThese.forEach(function (val, ind) {
 		t.similar(aMath.factors(val), checkAgainst[ind],
 			'correct arr returned for '+val)
